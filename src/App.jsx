167bece3402ab85eb1383e13ede3087cb2704e4d
vicktoriafedosova <vicktoriafedosova@yandex.by>
2019-03-20 14:23:22 +0300

    I've added my first commit

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <div>
+        <Header />
+      <Technologies/>  
+    </div>);
+}
+
+const  Technologies = () => {
+  return (
+  <div>
+      <ul>
+         <li>css</li>
+         <li>html</li>
+         <li>js</li>
+         <li>react</li>
+     </ul>
+    </div>
+  )
+}
+
+const Header = () => {
+  return (<div>
+    <a href='#s'>home</a>
+    <a href='#s'>New Feed</a>
+    <a href='#s'>Messages</a>
+  </div>);
+}
+export default App;
